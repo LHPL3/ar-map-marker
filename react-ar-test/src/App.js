9554b7189be8475a487970c5252a0049114fc4b5
LHPL3 <aaronbritt12@googlemail.com>
2021-07-23 16:13:31 +0100

    tsdf

--- react-ar-test/src/App.js
+++ react-ar-test/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a-scene
+        vr-mode-ui="enabled: false"
+        embedded
+        arjs="sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;"
+      >
+        <a-camera gps-camera rotation-reader></a-camera>
+
+        <a-entity
+          gltf-model="./magnemite/scene.gltf"
+          rotation="0 180 0"
+          scale="0.50 0.50 0.50"
+          gps-entity-place="longitude: 1.231280; latitude: 52.675541;"
+          animation-mixer
+        />
+      </a-scene>
     </div>
   );
 }
