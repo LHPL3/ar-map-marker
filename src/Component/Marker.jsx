1233280cdc5fd16b48746d5349ae65aa7e1aa669
LHPL3 <aaronbritt12@googlemail.com>
2021-07-26 14:18:05 +0100

    test

--- src/Component/Marker.jsx
+++ src/Component/Marker.jsx
@@ -1,29 +1,23 @@
 import React from 'react';
 import { Entity, Scene } from 'aframe-react';
-import ReactDOM from 'react-dom';
 
 const Marker = () => {
-  const success = (position) => {
-    console.log(position);
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-  };
-
-  navigator.geolocation.getCurrentPosition(success);
-
   return (
-    <Scene>
-      <Entity
-        geometry={{ primitive: 'box' }}
-        material={{ color: 'red' }}
-        position={{ x: 0, y: 0, z: -5 }}
-      />
-      <Entity particle-system={{ preset: 'snow' }} />
-      <Entity light={{ type: 'point' }} />
-      <Entity gltf-model={{ src: 'virtualcity.gltf' }} />
-      <Entity text={{ value: 'Hello, WebVR!' }} />
-    </Scene>
+    <a-scene
+      vr-mode-ui="enabled: false"
+      embedded
+      arjs="sourceType: webcam; debugUIEnabled: false;"
+    >
+      <a-text
+        value="This content will always face you."
+        look-at="[gps-camera]"
+        scale="120 120 120"
+        gps-entity-place="latitude: <add-your-latitude>; longitude: <add-your-longitude>;"
+      ></a-text>
+      <a-camera gps-camera rotation-reader>
+        {' '}
+      </a-camera>
+    </a-scene>
   );
 };
-
 export default Marker;
