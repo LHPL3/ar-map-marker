da4876aea0fe12fa6c7442fb10887060ccb77b9c
LHPL3 <aaronbritt12@googlemail.com>
2021-07-26 12:12:14 +0100

    test app

--- src/Component/Marker.jsx
+++ src/Component/Marker.jsx
@@ -10,18 +10,41 @@ const Marker = () => {
   navigator.geolocation.getCurrentPosition(success);
 
   return (
+    // <a-scene
+    //   vr-mode-ui="enabled: false"
+    //   embedded
+    //   arjs="sourceType: webcam; debugUIEnabled: false;"
+    // >
+    //   <a-text
+    //     value="This content will always face you."
+    //     look-at="[gps-camera]"
+    //     scale="50 50 50"
+    //     gps-entity-place="latitude: 52.675541; longitude: 1.231280;"
+    //   ></a-text>
+    //   <a-camera gps-camera rotation-reader></a-camera>
+    // </a-scene>
     <a-scene
       vr-mode-ui="enabled: false"
+      renderer="logarithmicDepthBuffer: true;"
       embedded
+      loading-screen="enabled: false;"
       arjs="sourceType: webcam; debugUIEnabled: false;"
     >
-      <a-text
-        value="This content will always face you."
-        look-at="[gps-camera]"
-        scale="50 50 50"
-        gps-entity-place="latitude: 52.675541; longitude: 1.231280;"
-      ></a-text>
       <a-camera gps-camera rotation-reader></a-camera>
+      <a-assets>
+        <a-asset-item
+          id="animated-asset"
+          src="assets/asset.gltf"
+        ></a-asset-item>
+      </a-assets>
+
+      <a-entity
+        look-at="[gps-camera]"
+        animation-mixer="loop: repeat"
+        gltf-model='"#animated-asset"'
+        scale="0.08861318584412939 0.08861318584412939 0.08861318584412939"
+        gps-entity-place="latitude: 52.675541; longitude: 1.23128;"
+      ></a-entity>
     </a-scene>
   );
 };
