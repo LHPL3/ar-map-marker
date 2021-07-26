319595f0ab103a9d58bff6675ecaa227bcfa99cf
LHPL3 <aaronbritt12@googlemail.com>
2021-07-26 14:25:22 +0100

    asd

--- src/Component/Marker.jsx
+++ src/Component/Marker.jsx
@@ -3,21 +3,21 @@ import { Entity, Scene } from 'aframe-react';
 
 const Marker = () => {
   return (
-    <a-scene
-      vr-mode-ui="enabled: false"
-      embedded
-      arjs="sourceType: webcam; debugUIEnabled: false;"
-    >
-      <a-text
-        value="This content will always face you."
-        look-at="[gps-camera]"
-        scale="120 120 120"
-        gps-entity-place="latitude: <add-your-latitude>; longitude: <add-your-longitude>;"
-      ></a-text>
-      <a-camera gps-camera rotation-reader>
-        {' '}
-      </a-camera>
-    </a-scene>
+    <div>
+      <a-scene
+        vr-mode-ui="enabled: false"
+        embedded
+        arjs="sourceType: webcam; debugUIEnabled: false;"
+      >
+        <a-text
+          value="PLZ"
+          look-at="gps-camera"
+          scale="0.5 0.5 0.5"
+          gps-entity-place="latitude: 52.675541; longitude: 1.231280;"
+        ></a-text>
+        <a-camera gps-camera rotation-reader></a-camera>
+      </a-scene>
+    </div>
   );
 };
 export default Marker;
